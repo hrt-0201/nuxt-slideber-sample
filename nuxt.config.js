2fccb815d7103c750e2dcefcedf87e7ecf15a900
hrt-0201 <tmyk.smtk@gmail.com>
2022-03-23 17:07:54 +0900

    vuetyfy commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,10 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    {
+      src: '@/plugins/plugin',
+      mode: 'client'
+    }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -29,6 +33,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/vuetify'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
